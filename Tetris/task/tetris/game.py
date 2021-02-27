b80dfef7d89e6c8b2bdc2b57c64a48dd160a8bcc
IgorBashkov <bashkov.torvm36@gmail.com>
2021-02-27 22:17:04 +0300

    4 stage process

--- Tetris/task/tetris/game.py
+++ Tetris/task/tetris/game.py
@@ -35,11 +35,13 @@ class Piece:
         self.board_x_right = 0
         self.max_y = 10
         self.board_y = 10
+        self.floor = self.size[0]
+        self.real_width = self.size[1]
         self.turn()
         # self.get_board_y()
 
     def get_board_y(self):
-        self.board_y = self.max_y - max(np.nonzero(self.array)[0]) - 1
+        self.board_y = self.max_y - self.floor - 1
 
     def turn(self):
         self.array[np.nonzero(self.array)] = False
@@ -47,6 +49,8 @@ class Piece:
             self.array[i] = True
         self.board_x_left = 0 - min(np.nonzero(self.array)[1])
         self.board_x_right = self.size[1] - max(np.nonzero(self.array)[1]) - 1
+        self.floor = max(np.nonzero(self.array)[0])
+        self.real_width = max(np.nonzero(self.array)[1]) - min(np.nonzero(self.array)[1])
         self.get_board_y()
         self.down()
 
@@ -64,7 +68,7 @@ class Piece:
         if self.cursor_y != self.board_y:
             self.cursor_y += 1
 
-    def move(self, direction):
+    """def move(self, direction):
         move_dict = {
             'rotate': self.turn,
             'down': self.down,
@@ -72,7 +76,7 @@ class Piece:
             'right': self.right,
         }
         if self.cursor_y != self.board_y:
-            move_dict[direction]()
+            move_dict[direction]()"""
 
     def __repr__(self):
         return '\n'.join([' '.join(map(lambda e: '-' if not e else '0', i)) for i in self.array])
@@ -84,40 +88,75 @@ class Field:
         self.height = height
         self.field = np.zeros((height, width), dtype=bool)
         self.result = np.copy(self.field)
-        self.piece = None
+        self.piece = Piece()
+        self.stop = False
+        self.over = False
 
     def add_piece(self, piece):
         self.piece = piece
+        self.stop = False
         self.piece.max_y = self.height
         self.piece.get_board_y()
         self.update()
 
-    def update(self):
+    def clear_field(self):
         self.field[np.nonzero(self.field)] = False
-        for n, i in enumerate(range(self.piece.cursor_y, self.piece.cursor_y + self.piece.size[0])):
-            for m, j in enumerate(range(self.piece.cursor_x, self.piece.cursor_x + self.piece.size[1])):
-                self.field[i % self.height, j % self.width] = self.piece.array[n, m]
+
+    def add_piece_from_input(self):
+        self.add_piece(Piece(input()))
+
+    def command(self, command):
+        move_dict = {
+            'rotate': self.piece.turn,
+            'down': self.piece.down,
+            'left': self.piece.left,
+            'right': self.piece.right,
+            'piece': self.add_piece_from_input,
+        }
+        if not self.stop or command == 'piece':  # self.piece.cursor_y != self.piece.board_y:
+            move_dict[command]()
+
+    def remove_complete(self):
+        for n, line in enumerate(self.result):
+            if np.zeros(line):
+                pass
+
+
+    def update(self):
+        self.clear_field()
+        if not self.stop:
+            for n, i in enumerate(range(self.piece.cursor_y, self.piece.cursor_y + self.piece.size[0])):
+                for m, j in enumerate(range(self.piece.cursor_x, self.piece.cursor_x + self.piece.size[1])):
+                    self.field[i % self.height, j % self.width] = self.piece.array[n, m]
+        if np.nonzero(self.field[-1])[0].size > 0 or np.nonzero(self.field[:-1] & self.result[1:])[0].size > 0:
+            self.result = self.field | self.result
+            self.clear_field()
+            self.stop = True
+            if np.nonzero(self.result[0])[0].size > 0:
+                self.over = True
 
     def __repr__(self):
-        return '\n'.join([' '.join(map(lambda e: '-' if not e else '0', i)) for i in self.field])
+        return '\n'.join([' '.join(map(lambda e: '-' if not e else '0', i)) for i in self.field | self.result])
 
 
 def run():
-    piece_type = input()
     field_size = tuple(map(int, input().split()))
-    print()
-    piece = Piece(piece_type)
+    # print()
+
     field = Field(*field_size)
 
     print(field, end='\n\n')
-    field.add_piece(piece)
-    print(field, end='\n\n')
+    # field.add_piece_from_input()
+    # print(field, end='\n\n')
     command = input()
-    while command != 'exit':
-        field.piece.move(command)
+    while command != 'exit' and not field.over:
+        field.command(command)
         field.update()
         print(field, end='\n\n', sep='')
         command = input()
+    if field.over:
+        print(field, end='\n\n', sep='')
+        print('Game Over!')
 
 
 if __name__ == '__main__':
